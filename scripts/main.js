3368a04835946932d9159c961444d75d2035caca
Mohammed Yaseen <mailofyaseen@gmail.com>
2020-05-19 18:30:54 +0100

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+// Creates the game board
+const createBoard = () => {
+  // Creates table element
+  const table = document.createElement('table');
+
+  // Sets the id of the board
+  table.setAttribute('id', 'board');
+
+  for (let i = 0; i < 3; i++) {
+    // Creates a table row
+    const tableRow = document.createElement('tr');
+
+    for (let j = 0; j < 3; j++) {
+      // Creates a table cell with text
+      const tableCell = document.createElement('td');
+      const cellContent = document.createTextNode('X');
+      tableCell.appendChild(cellContent);
+
+      // Appends cells to row
+      tableRow.append(tableCell);
+    }
+
+    // Appens table row to the table
+    table.appendChild(tableRow);
+  }
+
+  // Outputs table to the web page
+  const container = document.querySelector('.container');
+  container.appendChild(table);
+};
+
+createBoard();
