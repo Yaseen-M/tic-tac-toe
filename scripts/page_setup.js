c609b7b047160c88f20ba53d9b2b7ea185907cb0
Mohammed Yaseen <mailofyaseen@gmail.com>
2020-05-19 20:31:33 +0100

    Update cell text when clicked

--- scripts/page_setup.js
+++ scripts/page_setup.js
@@ -1,6 +1,7 @@
 class Game {
   // Creates game
   constructor() {
+    // Creates game attributes
     this.board = [
       ['', '', ''],
       ['', '', ''],
@@ -8,6 +9,7 @@ class Game {
     ];
     this.playerOneSymbol = 'X';
     this.playerTwoSymbol = '0';
+    this.turn = 0;
     this.winner = null;
     this.createBoard();
   }
@@ -29,7 +31,7 @@ class Game {
         const tableCell = document.createElement('td');
 
         // Sets the id of the cell as it coords
-        tableCell.setAttribute('id', `${i}-${j}`);
+        tableCell.setAttribute('id', `cell-${i}-${j}`);
 
         // Appends cells to row
         tableRow.append(tableCell);
@@ -43,6 +45,75 @@ class Game {
     const container = document.querySelector('.container');
     container.appendChild(table);
   }
+
+  // Changes the value of the clicked cell
+  cellClick(cellID) {
+    // Gets coords from cell ID
+    const x = cellID[5];
+    const y = cellID[7];
+
+    // Checks if cell is empty
+    if (this.board[x][y] === '') {
+      // If empty the cell is filled with the player's symbol
+      this.board[x][y] =
+        this.turn === 0 ? this.playerOneSymbol : this.playerTwoSymbol;
+      // Visible board is updated
+      this.updateBoard();
+      // Changes the current turn
+      this.turn = Number(!this.turn);
+    }
+  }
+
+  // Updates visible board
+  updateBoard() {
+    // Iterates through rows and columns
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Replaces visibile board text with game object board text
+        const cell = document.querySelector(`#cell-${i}-${j}`);
+        cell.textContent = this.board[i][j];
+      }
+    }
+  }
+
+  clearBoard() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        this.board[i][j] = '';
+      }
+    }
+    this.updateBoard();
+  }
 }
 
+// Creates game object
 const game = new Game();
+
+// Cell click event listeners
+document.querySelector('#cell-0-0').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
+document.querySelector('#cell-0-1').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
+document.querySelector('#cell-0-2').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
+document.querySelector('#cell-1-0').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
+document.querySelector('#cell-1-1').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
+document.querySelector('#cell-1-2').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
+document.querySelector('#cell-2-0').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
+document.querySelector('#cell-2-1').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
+document.querySelector('#cell-2-2').onclick = function () {
+  game.cellClick(this.getAttribute('id'));
+};
