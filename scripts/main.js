00ebd4568b4e542b322d8fab9049e7e4a45042d7
Mohammed Yaseen <mohammedysn15@gmail.com>
2020-05-20 19:01:21 +0100

    Fixed restarting on ties

--- scripts/main.js
+++ scripts/main.js
@@ -69,7 +69,21 @@ class Game {
       this.updateBoard();
 
       // Checks if game is over
-      this.isGameOver();
+      let gameOver = false;
+      if (this.isTie()) {
+        this.winnerUpdate(true);
+        gameOver = true;
+      } else if (this.isWinner()) {
+        this.winnerUpdate(false);
+        gameOver = true;
+      }
+
+      if (gameOver) {
+        this.showEndMsg();
+        this.restart();
+        console.log(this.players[0]['score']);
+        console.log(this.players[1]['score']);
+      }
 
       // Changes the current turn
       this.turn = Number(!this.turn);
@@ -88,25 +102,53 @@ class Game {
     }
   }
 
+  winnerUpdate(noWinner) {
+    if (noWinner) {
+      this.winner = 'tie';
+    } else {
+      this.winner = this.turn;
+      this.players[this.winner]['score'] += 1;
+    }
+  }
+
+  showEndMsg() {
+    Swal.fire({
+      titleText:
+        this.winner === 'tie'
+          ? `It's a ${this.winner}!`
+          : `Player ${this.winner + 1} wins!`,
+      timer: 3000,
+      showConfirmButton: false,
+      timerProgressBar: true,
+    });
+  }
+
+  isTie() {
+    let cellsFilled = 0;
+    this.gridCoords.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell !== '') {
+          cellsFilled++;
+        }
+      });
+    });
+    if (cellsFilled === 9) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   // Checks if game is over
-  isGameOver() {
+  isWinner() {
     if (
       this.checkVertical() ||
       this.checkHorizontal() ||
       this.checkDiagonal()
     ) {
-      this.winner = this.turn;
-      this.players[this.winner]['score'] += 1;
-      Swal.fire({
-        titleText:
-          this.winner === 'tie'
-            ? `It's a ${this.winner}!`
-            : `Player ${this.winner + 1} wins!`,
-        timer: 3000,
-        showConfirmButton: false,
-        timerProgressBar: true,
-      });
-      this.restart();
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -165,8 +207,8 @@ class Game {
         this.gridCoords[i][j] = '';
       }
     }
-    this.winner = this.turn;
-    console.log('Winner: ' + this.winner);
+    // this.winner = this.turn;
+    // console.log('Winner: ' + this.winner);
     this.updateBoard();
   }
 
